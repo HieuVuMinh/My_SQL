0e3bb1df818ab6a2706ec62f3ff46799e634ed63
unknown <vuhieu628383@gmail.com>
2021-05-18 10:28:07 +0700

    first commit

---
+++ 1. Cơ sở dữ liệu quan hệ/school.sql
@@ -0,0 +1,17 @@
+create database school;
+
+use school;
+
+CREATE TABLE class (
+    id_class INT,
+    name_class VARCHAR(255)
+);
+    
+CREATE TABLE teacher (
+    id_teacher INT,
+    name_teacher VARCHAR(255),
+    age INT,
+    country VARCHAR(255)
+);
+
+
