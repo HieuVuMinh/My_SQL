b873be386c438cee66bbf2e35fa6ebaba9be7c6d
unknown <vuhieu628383@gmail.com>
2021-05-19 10:58:35 +0700

    add file

---
+++ 2. Thiết kế và tạo CSDL/Them du lieu co so du lieu quan ly sinh vien.sql
@@ -0,0 +1,20 @@
+insert class
+values (1, "A1", "20081220", 1);
+INSERT INTO Class
+VALUES (2, 'A2', '2008-12-22', 1);
+INSERT INTO Class
+VALUES (3, 'B3', current_date, 0);
+
+insert subject 
+values (1, 'CF', 5, 1),
+		(2, 'C', 6, 1),
+       (3, 'HDJ', 5, 1),
+       (4, 'RDBMS', 10, 1);
+
+insert student 
+values (1, 'Hieu', 'HN', '0129512345', 1, 1);
+
+INSERT quanlysinhvien.mark
+VALUES (1, 1, 1, 8, 1),
+       (2, 3, 1, 10, 2),
+       (3, 2, 1, 12, 1);
